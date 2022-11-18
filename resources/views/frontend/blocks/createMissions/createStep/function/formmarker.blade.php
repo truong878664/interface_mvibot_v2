549cad57e5936479b97c4f2b4b928a754e5c492e
truong878664 <trandao056@gmail.com>
2022-11-18 16:12:21 +0700

    marker form

--- resources/views/frontend/blocks/createMissions/createStep/function/formmarker.blade.php
+++ resources/views/frontend/blocks/createMissions/createStep/function/formmarker.blade.php
@@ -20,12 +20,45 @@
 <input name="marker" type="radio" id="none-marker-dis">
 <input name="marker" type="radio" id="none-marker-angle">
 
-<div class="form-l-marker">
-    <form action="">
+<div class="form-marker-wrapper form-l-marker">
+    <button class="close-form-marker-btn"><i class="fa-solid fa-xmark"></i></button>
+    <div class="marker-title">L Marker</div>
+    <div class="wrapper-marker">
+        <form class="form-marker-item form-marker" action="">
+            <div class="offset-wrapper-all">
+                <div class="offset-wrapper-input">
+                    <label for="">Marker dir</label>
+                    <select name="marker_dir" class="input-offset">
+                        <option value=""></option>
+                        <option value="font_ward">Font ward</option>
+                        <option value="back_ward">Back ward</option>
+                    </select>
+                </div>
 
-    </form>
-    <div class="illustration">
-        123
+                <div class="offset-wrapper-input">
+                    <label for="">Offset X1</label>
+                    <input class="input-offset" type="text">
+                </div>
+                <div class="offset-wrapper-input">
+                    <label for="">Offset X2</label>
+                    <input class="input-offset" type="text">
+                </div>
+
+                <div class="offset-wrapper-input">
+                    <label for="">Offset Y1</label>
+                    <input class="input-offset" type="text">
+                </div>
+                <div class="offset-wrapper-input">
+                    <label for="">Offset Y2</label>
+                    <input class="input-offset" type="text">
+                </div>
+            </div>
+            <button class="submit-btn submit-marker">Add</button>
+
+        </form>
+        <div class="form-marker-item illustration">
+            <img src="/img/markerl.png" alt="">
+        </div>
     </div>
 </div>
 
