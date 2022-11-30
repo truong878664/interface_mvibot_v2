54412daf905231db3c69a51cd4e1fe7cdadc7d99
truong878664 <trandao056@gmail.com>
2022-11-30 17:31:14 +0700

    notthign

--- resources/views/frontend/pages/missions/createMissions.blade.php
+++ resources/views/frontend/pages/missions/createMissions.blade.php
@@ -1,7 +1,6 @@
 @extends('frontend.layouts.mainLayout')
 @section('content')
     <div class="heading missions-heading">Mission</div>
-    {{-- <div class="contents"> --}}
 
     <div class="contents missions-content">
         @include('frontend.blocks.navbarmissions')
@@ -11,8 +10,6 @@
         <script type="module" src="/js/missions.js"></script>
         <script type="module" src="/js/missions/createMissions.js"></script>
     </div>
-
-    {{-- </div> --}}
 
     <script>
         document.title = 'Create Mission | Mvibot'
