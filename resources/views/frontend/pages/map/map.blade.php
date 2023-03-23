468364625430be334a961b1467fe296b32320acc
truong878664 <trandao056@gmail.com>
2023-03-23 09:13:02 +0700

    change img map

--- resources/views/frontend/pages/map/map.blade.php
+++ resources/views/frontend/pages/map/map.blade.php
@@ -1,10 +1,37 @@
 @extends('frontend.layouts.mainLayout')
 @section('content')
     <div class="heading map-heading">Map</div>
-    <div class="w-full flex flex-wrap px-4 ">
-        <x-button tag="a" title="Manage map" class=""
+        {{-- <x-button tag="a" title="Manage map" class=""
             attribute="href={{ route('dashboard.map.choose-map-active') }}"></x-button>
         <x-button tag="a" title="Create Layer" class="" attribute="href={{ route('dashboard.map.create-layer') }}">
-        </x-button>
-    </div>
+        </x-button> --}}
+
+
+        <ul class="w-full flex flex-wrap  px-4">
+            <li class="w-full md:w-1/3 lg:w-1/5 xl:1/6 btn bg-opacity-70">
+                <a href="{{route('dashboard.map.choose-map-active') }}"
+                    class="w-[calc(100%_-_10px)] h-[140px] rounded-md p-4 mb-4 mx-[5px]  bg-cover shadow-md"
+                    style="background-image: url(/img/map/choose_map.png)">
+                    <div
+                        class=" flex relative items-end w-full h-full text-[#0f6cbd] href-mission text-5xl font-bold capitalize">
+                        <span class="text-shadown">
+                            Manage map
+                        </span>
+                    </div>
+                </a>
+            </li>
+
+
+            <li class="w-full md:w-1/3 lg:w-1/5 xl:1/6 ">
+                <a href="{{route('dashboard.map.create-layer') }}" style="background-image: url(/img/map/layer.png)"
+                    class="  w-[calc(100%_-_10px)] h-[140px] rounded-md p-4 mb-4 mx-[5px] bg-cover bg-no-repeat btn shadow-md">
+                    <div
+                        class=" flex relative items-end w-full h-full text-[#0f6cbd] href-mission text-5xl font-bold capitalize">
+                        <span class="text-shadown">
+                            Create layer
+                        </span>
+                    </div>
+                </a>
+            </li>
+        </ul>
 @endsection
