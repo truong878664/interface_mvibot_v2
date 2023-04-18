4b7af3ac312ada0de3bce565110dd1bfa9eb903b
truong878664 <trandao056@gmail.com>
2023-04-18 13:08:14 +0700

    newave

--- resources/views/frontend/pages/home/home.blade.php
+++ resources/views/frontend/pages/home/home.blade.php
@@ -3,7 +3,7 @@
     <div class="w-[calc(100%_-_10px)] h-[calc(100%_-_10px)] m-2 rounded-lg overflow-y-auto overflow-x-hidden">
         <div class="w-full h-full bg-cover flex relative items-end" style="background-image: url(/img/home2/bg.jpg)">
             <span class="absolute top-0 left-4 text-orange-400 text-2xl">website: maruei-robots.com</span>
-            <span class="absolute top-0 right-4 text-xl">version: mr 1.0</span>
+            <span class="absolute top-0 right-4 text-xl">version: nw 1.0</span>
             <div class="absolute top-[20%] left-1/2 -translate-x-1/2 flex flex-col items-center text-[#fff]">
                 <h1>
                     <div class="animation-text text-[100px] font-bold">
