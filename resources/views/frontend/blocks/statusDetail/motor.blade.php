dfe2d35234dc5ab73b9d0133532bbc31cabd5f7f
truong <trandao056@gmail.com>
2023-05-24 10:47:31 +0700

    color error status motor change

--- resources/views/frontend/blocks/statusDetail/motor.blade.php
+++ resources/views/frontend/blocks/statusDetail/motor.blade.php
@@ -15,14 +15,14 @@
     <span class="py-2 h-[30px] text-2xl pr-8 pl-2 text-transparent">-</span>
     <div class="flex flex-col motor_left">
         <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 live ">-</span>
-        <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 error">-</span>
+        <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 error [&.yes]:!text-red-600 [&.no]:!text-green-600">-</span>
         <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 enable">-</span>
         <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 brake">-</span>
     </div>
     <span class="py-2 h-[30px] text-2xl pr-8 pl-2 text-transparent">-</span>
     <div class="flex flex-col motor_right">
         <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 live">-</span>
-        <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 error">-</span>
+        <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 error [&.yes]:!text-red-600 [&.no]:!text-green-600">-</span>
         <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 enable">-</span>
         <span data-parameter-status="" data-parameter-title="motor" class="py-2 h-[30px] text-2xl pr-8 pl-2 brake">-</span>
     </div>
