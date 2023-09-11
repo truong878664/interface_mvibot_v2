c07ca7eebfb3fe2597b49a9d6b75103a504877fc
truong878664 <tranquoctruong307@gmail.com>
2023-09-11 17:03:56 +0700

    change type database start table

--- database/migrations/2023_08_22_184035_create_start_table.php
+++ database/migrations/2023_08_22_184035_create_start_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('start', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string("position_with_toollift");
-            $table->string("position_no_toollift");
-            $table->string("mission_go_to_toollift");
-            $table->string("missions_send_robot");
+            $table->mediumText("position_with_toollift");
+            $table->mediumText("position_no_toollift");
+            $table->mediumText("mission_go_to_toollift");
+            $table->mediumText("missions_send_robot");
         });
     }
 
