39dd2bcb6310fdf9ed87e85889f0450b2e72c892
truong878664 <tranquoctruong307@gmail.com>
2023-10-18 16:21:52 +0700

    add layer

--- public/js/map/layerV2.js
+++ public/js/map/layerV2.js
@@ -2,8 +2,17 @@ import Map from "../Object/Map.js";
 
 const getNode = document.querySelector.bind(document);
 const getNodeList = document.querySelectorAll.bind(document);
+const createLayer = getNode("#create-layer");
+const map = new Map({ mapID: "map", nameMap: "map_878664" });
 
 (function init() {
-    const map = new Map({ mapID: "map", nameMap: "map_878664" });
     map.create();
+    handleLayer();
 })();
+
+function handleLayer() {
+    createLayer.onchange = (e) => {
+        const is = e.target.checked;
+        is ? map.layer.set.create.enable() : map.layer.set.create.disable();
+    };
+}
--- resources/views/frontend/pages/map/createLayerV2.blade.php
+++ resources/views/frontend/pages/map/createLayerV2.blade.php
@@ -1,9 +1,24 @@
 @extends('frontend.layouts.mainLayout')
 @section('content')
     <div class="w-[calc(100%_-_10px)] h-[calc(100%_-_10px)] m-2 rounded-md overflow-hidden bg-stone-200 relative">
-        <div class="absolute top-0 left-0 w-full min-h-[40px] p-2">
-            <div class="bg-main/50 backdrop-blur-sm w-full h-full rounded-md p-2">
-                <input type="checkbox" class="w-6 h-6 rounded-md text-red-400">
+        <div class="absolute top-0 left-0 min-h-[40px] p-2">
+            <div class="backdrop-blur-sm h-full rounded-md flex bg-white shadow-md border">
+                <label>
+                    <input type="checkbox" class="sr-only peer/select" id="create-layer">
+                    <div
+                        class="px-3 py-2 text-2xl rounded-md text-black peer-checked/select:bg-stone-200 flex items-center gap-2">
+                        <i class="fa-solid fa-layer-group"></i>
+                        <span class="">Create layer</span>
+                    </div>
+                </label>
+                <label>
+                    <input type="checkbox" class="sr-only peer/select">
+                    <div
+                        class="px-3 py-2 text-2xl rounded-md text-black peer-checked/select:bg-stone-200 flex items-center gap-2">
+                        <i class="fa-solid fa-caret-down"></i>
+                        <span class="">List layer</span>
+                    </div>
+                </label>
             </div>
         </div>
         <div class="w-full h-full" id="map"></div>
