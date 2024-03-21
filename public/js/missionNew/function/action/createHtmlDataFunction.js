1d92d854610e1b0de4c97e7b3af6b45ede3c50be
truong878664 <tranquoctruong307@gmail.com>
2024-03-21 16:15:08 +0700

    fix lengt telegram

--- public/js/missionNew/function/action/createHtmlDataFunction.js
+++ public/js/missionNew/function/action/createHtmlDataFunction.js
@@ -2,7 +2,9 @@ import createHtmlFunctionItem from "../../component/Function/Function.js";
 import { htmlDataFunction } from "../index.js";
 
 export default function createHtmlDataFunction(data, type) {
-    htmlDataFunction[type].length = 0;
+    if (htmlDataFunction[type]) {
+        htmlDataFunction[type].length = 0;
+    }
 
     data.map((item, index) => {
         htmlDataFunction[type].push(createHtmlFunctionItem.create(item));
