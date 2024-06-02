dcdaf62c23bafb322909466dc6247b332ee85c94
Your Name <you@example.com>
2024-06-02 13:24:11 +0700

    fix: add header cors index php

--- public/index.php
+++ public/index.php
@@ -53,3 +53,8 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
+
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
